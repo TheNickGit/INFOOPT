7dcb673f40d2f3595bc0b892e6014b75e9c77fc3
justinsomechars <jjan.dirkp@gmail.com>
2022-11-25 14:51:23 +0100

    Parsing & Doubly linked list
    notes:
    - change data file-paths in 'Main' to absolute paths if running in debug mode (!!)
    - parsing is fully debugged; doubly-list might still contain little hiccups
    
    created:
    - Parsing.cs (Parser of data order and distance data)
    - Structures.cs (data-structures; Doubly linked list)
    - Models.cs (Order object)
    - data\Orderbestand.csv
    - data\AfstandenMatrix.csv
    
    removed:
    - textfiles of github tests in top directory
    - bin and obj directories for tidiness

---
+++ Infoopt/Infoopt/Models.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+
+namespace Infoopt
+{
+
+    class Order
+    {
+
+        public string place;            // place name of order
+        public int nr,                  // trivial order id
+            freq,                       // weekly frequency of order
+            binAmt,                     // amount of bins
+            binVol,                     // volume of bins
+            distId;                     // id of distance of order in distance-matrix (NOTE, DIFFERENT ORDERS MIGHT HAVE SAME DISTANCE ID ENTRY)
+        public float emptyDur;          // time it takes to empty the bins of this order
+        public (int X, int Y) coord;    // coordinates of order
+
+
+        // distances to all other orders (index = distId of other order)
+        public (int dist, int travelDur)[] distancesToOthers;
+
+        // instead use this helper method to get distance-data from one order to another order
+        public (int dist, int travelDur) distanceTo(Order other)
+            => this.distancesToOthers[other.distId];
+
+
+        public Order(
+                int nr, string place, int freq,
+                int binAmt, int binVol, float emptyDur,
+                int distId, int xCoord, int yCoord
+            )
+        {
+            this.nr = nr;
+            this.place = place;
+            this.freq = freq;
+            this.binAmt = binAmt;
+            this.binVol = binVol;
+            this.emptyDur = emptyDur;
+            this.distId = distId;
+            this.coord = (xCoord, yCoord);
+        }
+
+        // TODO: replace by console-input line format for route checker?
+        public override string ToString()
+        {
+            return String.Format("{0} | {1} [freq:{2}, amt:{3}, vol:{4}]",
+                this.distId.ToString().PadLeft(4),
+                this.place.PadRight(24),
+                this.freq.ToString().PadLeft(2),
+                this.binAmt.ToString().PadLeft(2),
+                this.binVol.ToString().PadLeft(4)
+            );
+        }
+
+    }
+}
--- Infoopt/Infoopt/Program.cs
+++ Infoopt/Infoopt/Program.cs
@@ -1,11 +1,52 @@
 ﻿using System;
 
+namespace Infoopt {
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello World!");
+    class Program {
+
+        static void Main(string[] args) {
+
+            // parse orders and display them
+            string orderFilePath = "./data/Orderbestand.csv"; // CHANGE TO ABSOLUTE PATH IF RUNNING IN DEBUG MODE
+            string distancesFilePath = "./data/AfstandenMatrix.csv"; // CHANGE TO ABSOLUTE PATH IF RUNNING IN DEBUG MODE
+            Order[] orders = fetchOrders(orderFilePath, distancesFilePath);
+            foreach (Order order in orders) Console.WriteLine(order);
+
+
+            // make doubly-list and display each value
+            DoublyList<int> dll = DoublyList<int>.fromArray(new int[10] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 });
+            foreach (DoublyNode<int> node in dll)
+                Console.Write($"{node.value} ");
+
+        }
+
+        // get all orders and bind all distance-data to respective orders
+        public static Order[] fetchOrders(string orderFilePath, string distancesFilePath) {
+            // parse orders and distances
+            Order[] orders = Parser.parseOrders(orderFilePath, nOrders:1177);
+            (int, int)[][] distances = Parser.parseOrderDistances(distancesFilePath, nDistances:1099);
+
+            // link distances to orders
+            foreach(Order order in orders) 
+                order.distancesToOthers = distances[order.distId];
+            return orders;
+        }
+
+
+        // For debugging purposes (remove later)
+        public static void Assert(bool pred) {
+            if (!pred) throw new Exception("ASSERTION FAIL");
+        }
+
+
     }
+    
+
+
+
+
 }
+
+
+
 
